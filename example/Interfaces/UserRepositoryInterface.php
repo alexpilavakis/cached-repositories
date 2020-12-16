9ccebf99e4f4dffa85afb9eb193d35faf228bf8a
alex <alexpilavakis@hotmail.com>
2020-12-16 19:50:08 +0200

    adjust interface example to extend cachingDecoratorInterface

--- example/Interfaces/UserRepositoryInterface.php
+++ example/Interfaces/UserRepositoryInterface.php
@@ -2,59 +2,9 @@
 
 namespace App\Repositories\Interfaces;
 
-interface UserRepositoryInterface
+use Ulex\CachedRepositories\Interfaces\CachingDecoratorInterface;
+
+interface UserRepositoryInterface extends CachingDecoratorInterface
 {
-    /**
-     * @return mixed
-     */
-    public function getAll();
 
-    /**
-     * @param $id
-     * @return mixed
-     */
-    public function getById($id);
-
-    /**
-     * @param $attribute
-     * @param $value
-     * @return mixed
-     */
-    public function getBy($attribute, $value);
-
-    /**
-     * @param $id
-     * @return mixed
-     */
-    public function findOrFail($id);
-
-    /**
-     * @param $attributes
-     * @return mixed
-     */
-    public function create($attributes);
-
-    /**
-     * @param $attributes
-     * @return mixed
-     */
-    public function firstOrCreate($attributes);
-
-    /**
-     * @param $attributes
-     * @return mixed
-     */
-    public function updateOrCreate($attributes);
-
-    /**
-     * @param $model
-     * @param $attributes
-     * @return mixed
-     */
-    public function update($model, $attributes);
-
-    /**
-     * @param $model
-     */
-    public function delete($model);
 }
